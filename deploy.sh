daab91d64f7de6c92e91d0744e1f91203d82a3f9
Ardianta Pargo <ardianta_pargo@yahoo.co.id>
2018-01-01 13:36:57 +0800

    Publishing to gh-pages (deploy.sh)

--- deploy.sh
+++ deploy.sh
@@ -22,4 +22,7 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (deploy.sh)"
+cd public
+git add --all
+git commit -m "Publishing to gh-pages (deploy.sh)"
+git push origin gh-pages
