ffba5762c03a33f45c2f07a112da46582103f8c0
Ardianta Pargo <ardianta_pargo@yahoo.co.id>
2018-01-01 13:52:23 +0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -23,7 +23,6 @@ hugo
 
 echo "Updating gh-pages branch"
 cd public
-git checkout gh-pages
 git add --all
 git commit -m "Publishing to gh-pages (deploy.sh)"
 git push origin gh-pages
