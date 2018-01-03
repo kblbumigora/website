8e6a8eae5468f11132b7cffab20cc1f829e7c34c
Ardianta Pargo <ardianta_pargo@yahoo.co.id>
2018-01-03 15:40:52 +0800

    fix build script

--- deploy-ci.sh
+++ deploy-ci.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "kbl.bumigora@gmail.com"
+  git config --global user.name "KBL Bumigora"
 }
 
 build_website() {
+  echo "Deleting old publication"
+  rm -rf public
+  mkdir public
+  git worktree prune
+  rm -rf .git/worktrees/public/
+
+  echo "Checking out gh-pages branch into public"
+  git worktree add -B gh-pages public origin/gh-pages
+
+  echo "Removing existing files"
+  rm -rf public/*
+
+  echo "Generating site..."
   hugo
+  cd public
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
-  git add .
+  #git checkout -b gh-pages
+  git add --all
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
