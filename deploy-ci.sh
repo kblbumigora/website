abdab1305ca0bcab17c807bd09dcad1f241611aa
Ardianta Pargo <ardianta_pargo@yahoo.co.id>
2018-01-03 16:02:55 +0800

    fix build script

--- deploy-ci.sh
+++ deploy-ci.sh
@@ -8,22 +8,15 @@ fi
 
 echo "Deleting old publication"
 rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
-
-echo "Generating site"
+echo "Generating site..."
 hugo
+
 
 echo "Updating gh-pages branch"
 cd public
+git init
 git add --all
-git commit -m "Publishing to gh-pages (deploy.sh)"
-git remote add origin-pages https://${GITHUB_TOKEN}@github.com/kblbumigora/website.git > /dev/null 2>&1
-git push --set-upstream origin gh-pages 
+git commit -m "Build from Travis CI"
+#git remote add origin-pages https://${GITHUB_TOKEN}@github.com/kblbumigora/website.git > /dev/null 2>&1
+git push --force "https://${GITHUB_TOKEN}@$github.com/kblbumigora/website.git" master:gh-pages > /dev/null 2>&1
